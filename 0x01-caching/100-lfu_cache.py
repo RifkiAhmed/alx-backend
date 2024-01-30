0c372ba866f32c9f09419dd3cc17b0547a3de316
ahmed <rifkiahmed.91@gmail.com>
2024-01-30 23:36:58 +0100

    Create LFUCache class, a caching system

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''LFUCache Model'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    '''LFUCache CLass'''
+    my_cache = {}
+
+    def put(self, key, item):
+        '''Add an item in the cache'''
+        if key and item:
+            keys = list(self.my_cache.keys())
+            if len(keys) >= BaseCaching.MAX_ITEMS:
+                min_value = min(self.my_cache.values())
+                for key, value in self.my_cache.items():
+                    if value == min_value:
+                        v_key = key
+                        break
+                del self.cache_data[v_key]
+                del self.my_cache[v_key]
+                print('DISCARD:', v_key)
+            self.cache_data[key] = item
+            self.my_cache[key] = 0
+
+    def get(self, key):
+        '''Get an item by key'''
+        if self.cache_data.get(key):
+            self.my_cache[key] += 1
+            return self.cache_data[key]
+        return None

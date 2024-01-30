950b6a1b300eed5787085fb4c8c61afc60bf296c
ahmed <rifkiahmed.91@gmail.com>
2024-01-30 21:32:18 +0100

    Create FIFOCache class, a caching system

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -11,7 +11,7 @@ class FIFOCache(BaseCaching):
         '''Add an item in the cache'''
         if key and item:
             keys = list(self.cache_data.keys())
-            if len(keys) >= BaseCaching.MAX_ITEMS:
+            if len(keys) > BaseCaching.MAX_ITEMS:
                 del self.cache_data[keys[0]]
                 print('DISCARD: ', keys[0])
             self.cache_data[key] = item

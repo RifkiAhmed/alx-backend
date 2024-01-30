bd40b5121a7d1fe69add99ec7406516de76c105d
ahmed <rifkiahmed.91@gmail.com>
2024-01-30 21:31:53 +0100

    Create FIFOCache class, a caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''BasicCache Model'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''FIFOCache CLass'''
+
+    def put(self, key, item):
+        '''Add an item in the cache'''
+        if key and item:
+            keys = list(self.cache_data.keys())
+            if len(keys) >= BaseCaching.MAX_ITEMS:
+                del self.cache_data[keys[0]]
+                print('DISCARD: ', keys[0])
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get an item by key'''
+        if self.cache_data.get(key):
+            return self.cache_data[key]
+        return None

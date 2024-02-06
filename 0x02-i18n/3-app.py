15f1eed27ff572b44a4369da1bc16e6b164865c1
ahmed <rifkiahmed.91@gmail.com>
2024-02-06 22:24:59 +0100

    Flask app

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Class Config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Render the index page"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Return the best locale that match the best with supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

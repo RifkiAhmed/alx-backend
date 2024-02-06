b9b127d0ec62f04260601d7340f97bb76d073e7b
ahmed <rifkiahmed.91@gmail.com>
2024-02-06 22:02:51 +0100

    Add documentation for the class Config

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Flask app'''
+"""Flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -8,6 +8,7 @@ babel = Babel(app)
 
 
 class Config:
+    """Class Config"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -16,11 +17,11 @@ class Config:
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """Render the index page"""
-    return render_template('1-index.html')
+    return render_template("1-index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
